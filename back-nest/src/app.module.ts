f483c6e7d2b437f69a3e12a6a74f4a065770c936
Kobra300Vs <konrad_wojda@wp.pl>
2024-06-08 19:48:48 +0200

    BE-adding-nest

---
+++ back-nest/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DrawingsModule } from './drawings/drawings.module';
+import { Drawings } from './drawings/drawings.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.db',
+      entities: [Drawings], //__dirname + '/../**/*.entity{.ts,.js}''
+      synchronize: true,
+    }),
+    DrawingsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
